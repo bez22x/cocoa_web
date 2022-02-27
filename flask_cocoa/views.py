ded5b0f2a544a3a7cba90c924a884fa15ad082d0
bez22x <enjoy333x@yahoo.com.tw>
2022-02-27 17:57:32 +0800

    add cart

--- flask_cocoa/views.py
+++ flask_cocoa/views.py
@@ -1,3 +1,5 @@
+from typing import Dict, Any, Union
+
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_cocoa import app
 from flask_cocoa import db
@@ -34,17 +36,51 @@ def show_hot():
 
 @app.route('/shopping_cart')
 def show_shopping_cart():
-    items = Product.query.all()[:5]
-    return render_template('entries/shopping_cart.html', items=items)
+    return render_template('entries/shopping_cart.html')
 
 @app.route('/add_cart', methods=["POST"])
 def add_cart():
-    if 'cart_item' not in session:
-        session['cart_item'] = 0
-    session['cart_item'] += int(request.form['quantity'])
-    return redirect(request.referrer)
+    item_quantity = int(request.form['quantity'])
+    item_id = request.form['id']
+    if item_quantity and item_id and request.method == 'POST':
+        item = Product.query.filter_by(product_ID=item_id).first()
+        item_dict = {
+            item_id: {'name': item.product_name, 'id': item.product_ID, 'picture': item.picture_URL,
+                         'quantity': item_quantity, 'price': item.product_price,
+                         'total_price': item_quantity * item.product_price}}
 
+        all_total_price = 0
+        all_total_quantity = 0
 
+        session.modified = True
+        if 'cart_item' in session:
+            if item_id in session['cart_item']:
+                for key, value in session['cart_item'].items():
+                    if item_id == key:
+                        session.modified = True
+                        old_quantity = session['cart_item'][key]['quantity']
+                        total_quantity = old_quantity + item_quantity
+                        session['cart_item'][key]['quantity'] = total_quantity
+                        session['cart_item'][key]['total_price'] = total_quantity * item.product_price
+            else:
+                session['cart_item'].update(item_dict)
+
+            for key, value in session['cart_item'].items():
+                individual_quantity = int(session['cart_item'][key]['quantity'])
+                individual_price = int(session['cart_item'][key]['total_price'])
+                all_total_quantity = all_total_quantity + individual_quantity
+                all_total_price = all_total_price + individual_price
+        else:
+            session['cart_item'] = item_dict
+            all_total_quantity = all_total_quantity + item_quantity
+            all_total_price = all_total_price + item_quantity * item.product_price
+
+        session['all_total_quantity'] = all_total_quantity
+        session['all_total_price'] = all_total_price
+
+        return redirect(request.referrer)
+    else:
+        return '購物車添加錯誤'
 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
